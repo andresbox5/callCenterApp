97682c5f384c168e6557c9825d2a0e45cbf2765d
FABIAN <andresbox5@gmail.com>
2018-08-08 01:44:31 -0500

    Contiene solución prueba Almundo

--- src/main/java/com/co/almundo/callcenter/config/SwaggerDocumentationConfig.java
+++ src/main/java/com/co/almundo/callcenter/config/SwaggerDocumentationConfig.java
@@ -1,4 +1,4 @@
-package co.com.almundo.callcenter.config;
+package com.co.almundo.callcenter.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
